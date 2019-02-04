8031bfdb4f3591181064b1a43ff6329b716f13bf
EyotZex <andykovalsky@hotmail.com>
2019-02-04 16:20:11 +0000

    Readable Objects Added

---
+++ Detective Game/Assets/Scripts/ReadableObject.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReadableObject : MonoBehaviour
+{
+    public bool interactRange = false;
+    public Transform Origin;
+    public LayerMask playerLayer;
+    public float maskSize;
+    public bool signActive;
+    public Text textBox;
+    public GameObject textBoxObject;
+    [TextArea]
+    public string textInfo;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        interactRange = Physics2D.OverlapCircle(Origin.position, maskSize, playerLayer);
+
+        if (interactRange)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (!signActive)
+                {
+                    textBoxObject.SetActive(true);
+                    textBox.text = textInfo;
+                    signActive = true;
+                }
+                else
+                {
+                    textBoxObject.SetActive(false);
+                    signActive = false;
+                }
+            }
+        }
+        else
+        {
+            textBoxObject.SetActive(false);
+            signActive = false;
+        }
+    }
+}
+
