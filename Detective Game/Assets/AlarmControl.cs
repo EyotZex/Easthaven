424b04ba97625beab4fe18dbad1ed547f78d7100
EyotZex <andykovalsky@hotmail.com>
2019-02-22 14:33:51 +0000

    chapter 4 rebuild

---
+++ Detective Game/Assets/AlarmControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlarmControl : MonoBehaviour
+{
+
+    public Alarm alarmScript;
+    public bool CanBeInteracted;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1) && CanBeInteracted)
+        {
+            if (alarmScript.alarmOn)
+            {
+                alarmScript.alarmOn = false;
+            }
+            else
+            {
+                alarmScript.alarmOn = true;
+            }
+        }
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            CanBeInteracted = true;
+        }
+    }
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            CanBeInteracted = false;
+        }
+    }
+}
+
+   
---
+++ Detective Game/Assets/Scripts/Alarm.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alarm : MonoBehaviour
+{
+    public bool alarmOn;
+    public AudioSource alarmAudioSource;
+    public AudioClip alarmSound;
+    public Transform soundOrigin;
+    public GameObject SoundObject;
+    public float timer;
+    public float timerStart;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (alarmOn)
+        {
+            timer -= Time.deltaTime;
+
+            if (timer < 0)
+            {
+                alarmAudioSource.Stop();
+                alarmAudioSource.PlayOneShot(alarmSound, 1.0f);
+                Instantiate(SoundObject, soundOrigin.transform.position, Quaternion.identity);
+                timer = timerStart;
+            }
+        }
+    }
+}
