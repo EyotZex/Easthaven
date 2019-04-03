5770ac328b134ea2c585f5257df26e2d102b8a69
EyotZex <andykovalsky@hotmail.com>
2019-04-03 16:24:18 +0100

    monster spawner

---
+++ Detective Game/Assets/MonsterSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSpawner : MonoBehaviour
+{
+    public GameObject[] monsters;
+    public GameObject monster;
+    private int index;
+    public float timer;
+    public float timerstart;
+
+    void Start()
+    {
+        timer = timerstart;
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if(timer <= 0)
+        {
+            index = Random.Range(0, monsters.Length);
+            monster = monsters[index];
+            monster.SetActive(true);
+            timer = timerstart;
+        }
+    }
+}
